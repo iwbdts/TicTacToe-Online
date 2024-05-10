585db7085d84e5d3a236ade3fc13cc5bd4c6a991
iwbdts <92332248+iwbdts@users.noreply.github.com>
2024-05-11 00:03:04 +0200

    const port

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ var rooms = {};
 var defaultGameState = ["", "", "", "", "", "", "", "", ""];
 
 // app.use(express.urlencoded({ extended: true }));
-
+const PORT = process.env.PORT || 3000;
 app.get("/", function (req, res) {
     res.render("index");
 });
@@ -342,6 +342,6 @@ io.on("connection", (socket) => {
     });
 });
 
-server.listen(3000, () => {
-    console.log("Server is running on port 3000");
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
 });
